0506d32e6ef9137dae8cc1095864d02eafe04e0f
Kesistance <sochi.dragon@yandex.ru>
2024-04-07 22:47:56 +0300

    Fix function's

--- Lab_4/js/script.js
+++ Lab_4/js/script.js
@@ -1,137 +1,131 @@
 function insertImage() {
 	document.querySelectorAll('.box').forEach(image => {
-		if (image.innerText.length !== 0) {
-			if (image.innerText == 'Wrawn' || image.innerText == 'Bpawn') {
-				image.innerHTML =
-					'${image.innerText} <img class = "allimg allpaw" src = "${image.innerText}.png" alt="">';
-				image.computedStyleMap.cursor = 'pointer';
-			} else {
-				image.innerHTML =
-					'${image.innerText} <img class="allimg" src="${image.innerText}.png" alt="">';
-				image.style.cursor = 'pointer';
+			if (image.innerText.length !== 0) {
+					if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+							image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+							image.style.cursor = 'pointer'
+					}
+					else {
+							image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png" alt="">`
+							image.style.cursor = 'pointer'
+					}
 			}
-		}
-	});
+	})
 }
-insertImage();
+insertImage()
+
 
 function coloring() {
-	const color = document.querySelectorAll('.box');
+	const color = document.querySelectorAll('.box')
 	color.forEach(color => {
-		getId = color.id;
-		arr = Array.from(getId);
-		arr.shift();
-		aside = eval(arr.pop());
-		aup = eval(arr.shift());
-		a = aside + aup;
+			getId = color.id
+			arr = Array.from(getId)
+			arr.shift()
+			aside = eval(arr.pop())
+			aup = eval(arr.shift())
+			a = aside + aup
 
-		if (a % 2 == 0) {
-			color.style.backgroundColor = 'rgb(240, 201, 150)';
-		}
-		if (a % 2 !== 0) {
-			color.style.backgroundColor = 'rgb(100, 75, 43)';
-		}
-	});
+			if (a % 2 == 0) {
+				color.style.backgroundColor = 'rgb(240, 201, 150)'
+			}
+			if (a % 2 !== 0) {
+				color.style.backgroundColor = 'rgb(100, 75, 43)'
+			}
+	})
 }
-coloring();
+coloring()
 
 function reddish() {
 	document.querySelectorAll('.box').forEach(i1 => {
-		if (i1.style.backgroundColor == 'pink') {
-			document.querySelectorAll('.box').forEach(i2 => {
-				if (i2.style.backgroundColor == 'green' && i2.innerText.length !== 0) {
-					greenText = i2.innerText;
-					pinkText = i1.innerText;
-					pinkColor = Array.from(pinkText).shift().toString();
-					greenColor = Array.from(greenText).shift().toString();
+			if (i1.style.backgroundColor == 'pink') {
+					document.querySelectorAll('.box').forEach(i2 => {
+							if (i2.style.backgroundColor == 'green' && i2.innerText.length !== 0) {
+									greenText = i2.innerText
+									pinkText = i1.innerText
+									pinkColor = ((Array.from(pinkText)).shift()).toString()
+									greenColor = ((Array.from(greenText)).shift()).toString()
+									getId = i2.id
+									arr = Array.from(getId)
+									arr.shift()
+									aside = eval(arr.pop())
+									aup = eval(arr.shift())
+									a = aside + aup
 
-					if (pinkColor == greenColor) {
-						i2.style.backgroundColor = 'rgb(253, 60, 60)';
-					}
-				}
-			});
-		}
-	});
+									if (a % 2 == 0 && pinkColor == greenColor) {
+										i2.style.backgroundColor = 'rgb(240, 201, 150)'
+									}
+									if (a % 2 !== 0 && pinkColor == greenColor) {
+										i2.style.backgroundColor = 'rgb(100, 75, 43)'
+									}
+							}
+					})
+			}
+	})
 }
 
-tog = 1;
+tog = 1
+whiteCastleChance = true
+blackCastleChance = true
 
 document.querySelectorAll('.box').forEach(item => {
 	item.addEventListener('click', function () {
-		if (item.style.backgroundColor == 'green' && item.innerText.length == 0) {
-			tog = tog + 1;
-		} else if (
-			item.style.backgroundColor == 'green' &&
-			item.innerText.length !== 0
-		) {
-			document.querySelectorAll('.box').forEach(i => {
-				if (i.style.backgroundColor == 'pink') {
-					pinkId = i.id;
-					pinkText = i.innerText;
-					document.getElementById(pinkId).innerText = '';
-					item.innerText = pinkText;
-					coloring();
-					insertImage();
-					tog = tog + 1;
-				}
-			});
-		}
-
-		getId = item.id;
-		arr = Array.from(getId);
-		arr.shift();
-		aside = eval(arr.pop());
-		arr.push('0');
-		aup = eval(arr.join(''));
-		a = aside + aup;
+			if (item.style.backgroundColor == 'green' && item.innerText.length == 0) {
+				tog = tog + 1
+			}
+			else if (item.style.backgroundColor == 'aqua' && item.innerText.length == 0) {
+				tog = tog + 1
+			}
+			else if (item.style.backgroundColor == 'green' && item.innerText.length !== 0) {
+					document.querySelectorAll('.box').forEach(i => {
+							if (i.style.backgroundColor == 'pink') {
+									pinkId = i.id
+									pinkText = i.innerText
+									document.getElementById(pinkId).innerText = ''
+									item.innerText = pinkText
+									coloring()
+									insertImage()
+									tog = tog + 1
+							}
+					})
+			}
+			
+			getId = item.id
+			arr = Array.from(getId)
+			arr.shift()
+			aside = eval(arr.pop())
+			arr.push('0')
+			aup = eval(arr.join(''))
+			a = aside + aup
 
 		function whosTurn(toggle) {
 			if (item.innerText == '${toggle}pawn') {
-				item.style.backgroundColor = 'pink';
+				item.style.backgroundColor = 'pink'
 
 				if (tog % 2 !== 0 && aup < 800) {
 					if (document.getElementById('b${a + 100}').innerText.length == 0) {
-						document.getElementById('b${a + 100}').style.backgroundColor =
-							'green';
+						document.getElementById('b${a + 100}').style.backgroundColor = 'green'
 					}
 
-					if (
-						aside < 8 &&
-						document.getElementById('b${a + 100 + 1}').innerText.length !== 0
-					) {
-						document.getElementById('b${a + 100 + 1}').style.backgroundColor =
-							'green';
+					if (aside < 8 && document.getElementById('b${a + 100 + 1}').innerText.length !== 0) {
+						document.getElementById('b${a + 100 + 1}').style.backgroundColor = 'green'
 					}
 
-					if (
-						aside > 1 &&
-						document.getElementById('b${a + 100 - 1}').innerText.length !== 0
-					) {
-						document.getElementById('b${a + 100 - 1}').style.backgroundColor =
-							'green';
+					if (aside > 1 &&	document.getElementById('b${a + 100 - 1}').innerText.length !== 0) {
+						document.getElementById('b${a + 100 - 1}').style.backgroundColor = 'green'
 					}
 				}
 
 				if (tog % 2 == 0 && aup > 100) {
 					if (document.getElementById('b${a - 100}').innerText.length == 0) {
-						document.getElementById('b${a - 100}').style.backgroundColor =
-							'green';
+						document.getElementById('b${a - 100}').style.backgroundColor = 'green'
 					}
 
-					if (
-						aside < 8 &&
-						document.getElementById('b${a - 100 + 1}').innerText.length !== 0
-					) {
-						document.getElementById('b${a - 100 + 1}').style.backgroundColor =
-							'green';
+					if (aside < 8 && document.getElementById('b${a - 100 + 1}').innerText.length !== 0) {
+						document.getElementById('b${a - 100 + 1}').style.backgroundColor = 'green'
 					}
 
-					if (
-						aside > 1 &&
-						document.getElementById('b${a - 100 - 1}').innerText.length !== 0
-					) {
-						document.getElementById('b${a - 100 - 1}').style.backgroundColor =
-							'green';
+					if (aside > 1 && document.getElementById('b${a - 100 - 1}').innerText.length !== 0) {
+						document.getElementById('b${a - 100 - 1}').style.backgroundColor = 'green'
 					}
 				}
 			}
@@ -540,5 +534,49 @@ document.querySelectorAll('.box').forEach(item => {
 				numOfKings += 1;
 			}
 		});
+
+		if (numOfKings == 1) {
+			setTimeout(() => {
+				if (tog % 2 == 0) {
+					alert('White wins !!');
+					location.reload();
+				} else if (tog % 2 !== 0) {
+					alert('Black Wins !!');
+					location.reload();
+				}
+			}, 100);
+		}
+	});
+});
+
+document.querySelectorAll('.box').forEach(hathiTest => {
+	hathiTest.addEventListener('click', function () {
+		if (hathiTest.style.backgroundColor == 'pink') {
+			pinkId = hathiTest.id;
+			pinkText = hathiTest.innerText;
+			document.querySelectorAll('.box').forEach(hathiTest2 => {
+				hathiTest2.addEventListener('click', function () {
+					if (
+						hathiTest2.style.backgroundColor == 'green' &&
+						hathiTest2.innerText.length == 0
+					) {
+						document.getElementById(pinkId).innerText = '';
+						hathiTest2.innerText = pinkText;
+						coloring();
+						insertImage();
+					}
+				});
+			});
+		}
+	});
+});
+
+z = 0;
+document.querySelectorAll('.box').forEach(ee => {
+	ee.addEventListener('click', function () {
+		z = z + 1;
+		if (z % 2 == 0 && ee.style.backgroundColor !== 'green') {
+			coloring();
+		}
 	});
 });
