b89d5758eb976583045a5079170401e5ff3ff337
Kesistance <sochi.dragon@yandex.ru>
2024-05-18 20:19:37 +0300

    All

--- Lab_6/Task1/RazorPagesApp/Models/Movie.cs
+++ Lab_6/Task1/RazorPagesApp/Models/Movie.cs
@@ -8,6 +8,7 @@ namespace RazorPagesApp.Models
         public int Id { get; set; }
         [StringLength(60, MinimumLength = 3)]
         [Required]
+        [Display(Name = "Название")]
         public string? Title { get; set; }
         [Display(Name = "Дата выпуска")]
         [DataType(DataType.Date)]
@@ -15,14 +16,17 @@ namespace RazorPagesApp.Models
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         [Required]
         [StringLength(30)]
+        [Display(Name = "Жанр")]
         public string? Genre { get; set; }
         [Range(1, 100)]
         [DataType(DataType.Currency)]
+        [Display(Name = "Цена")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         [StringLength(5)]
         [Required]
+        [Display(Name = "Рейтинг ")]
         public string Rating { get; set; } = string.Empty;
     }
 }
