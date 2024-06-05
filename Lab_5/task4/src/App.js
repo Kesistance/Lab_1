11c6305e34278a4e927aa39b579e05c4c8695764
Kesistance <sochi.dragon@yandex.ru>
2024-06-05 17:32:18 +0300

    task4-5

--- Lab_5/task4/src/App.js
+++ Lab_5/task4/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const navlinkItems = [
+		<li>
+			Link to <a href='https://www.google.com'>google.com</a>
+		</li>,
+		<li>
+			Link to <a href='https://www.facebook.com'>facebook.com</a>
+		</li>,
+		<li>
+			Link to <a href=' https://www.amazon.com'>amazon.com</a>
+		</li>,
+	];
+	return <ul className='nav'>{navlinkItems}</ul>;
 }
 
 export default App;
